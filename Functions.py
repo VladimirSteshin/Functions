8d3265272ab985d0007ad6990c7d35b6c3bf301e
Vladimir Steshin <warheim24@gmail.com>
2022-02-16 16:58:00 +0300

    Ending at work

--- Functions.py
+++ Functions.py
@@ -1,21 +1,28 @@
 from Functions_work import directories, documents
 
 
+def doc_people():
+    for ask in documents:
+        return ask
+
+
 def program(command=(input('Введите команду: ').lower())):
     if command == 'p':
         return doc_people()
-    elif command == 's':
-        return doc_shelf()
-    elif command == 'l':
-        return doc_list()
-    elif command == 'a':
-        return doc_add()
-    elif command == 'd':
-        return doc_delete()
-    elif command == 'm':
-        return doc_move()
-    elif command == 'as':
-        return doc_add_shelf()
+    # elif command == 's':
+    #     return doc_shelf()
+    # elif command == 'l':
+    #     return doc_list()
+    # elif command == 'a':
+    #     return doc_add()
+    # elif command == 'd':
+    #     return doc_delete()
+    # elif command == 'm':
+    #     return doc_move()
+    # elif command == 'as':
+    #     return doc_add_shelf()
     else:
-        print('Неверная команда! Введите команду ещё раз: ')
-        return program()
+        return program(command=(input('Неверная команда! Введите команду повторно: ').lower()))
+
+
+print(program())
