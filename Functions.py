7a100d68d7f4542409e06fdac11ddc1588ed8205
Vladimir Steshin <warheim24@gmail.com>
2022-02-16 15:56:12 +0300

    Base func formed

--- Functions.py
+++ Functions.py
@@ -1,2 +1,21 @@
 from Functions_work import directories, documents
 
+
+def program(command=(input('Введите команду: ').lower())):
+    if command == 'p':
+        return doc_people()
+    elif command == 's':
+        return doc_shelf()
+    elif command == 'l':
+        return doc_list()
+    elif command == 'a':
+        return doc_add()
+    elif command == 'd':
+        return doc_delete()
+    elif command == 'm':
+        return doc_move()
+    elif command == 'as':
+        return doc_add_shelf()
+    else:
+        print('Неверная команда! Введите команду ещё раз: ')
+        return program()
