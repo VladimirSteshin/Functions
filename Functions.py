0a49c6e71f14fa33bbc12fc0403c5de51c6c4f5e
Vladimir Steshin <warheim24@gmail.com>
2022-02-17 13:26:25 +0300

    Necessary work is done

--- Functions.py
+++ Functions.py
@@ -1,20 +1,55 @@
 from Functions_work import directories, documents
 
 
-def doc_people():
+def doc_people(doc_number):
+    result = 0
     for ask in documents:
-        return ask
+        if doc_number in ask.values():
+            result = ask['name']
+            return result
+    if result == 0:
+        return doc_people(input('Неверный номер! Введите номер документа повторно: '))
+
+
+def doc_shelf(doc_number):
+    result = 0
+    for key, value in directories.items():
+        if doc_number in value:
+            result = key
+            return f'Номер полки: {result}'
+    if result == 0:
+        return doc_shelf(input('Неверный номер! Введите номер документа повторно: '))
+
+
+def doc_list():
+    for ask in documents:
+        print(f'{ask["type"]} "{ask["number"]}" "{ask["name"]}"')
+    return ''
+
+
+def doc_add(add_shelf, add_type, add_number, add_name):
+    if add_shelf in directories.keys():
+        documents.append({'type': add_type, 'number': add_number, 'name': add_name})
+        for key, value in directories.items():
+            if key == add_shelf:
+                value.append(add_number)
+        return documents, directories
+    else:
+        print('Неверный номер полки!')
+        return doc_add(input('Введите номер полки для хранения: '), input('Введите тип документа: '),
+                       input('Введите номер документа: '), input('Введите имя владельца документа: '))
 
 
 def program(command=(input('Введите команду: ').lower())):
     if command == 'p':
-        return doc_people()
-    # elif command == 's':
-    #     return doc_shelf()
-    # elif command == 'l':
-    #     return doc_list()
-    # elif command == 'a':
-    #     return doc_add()
+        return doc_people(input('Введите номер документа: '))
+    elif command == 's':
+        return doc_shelf(input('Введите номер документа: '))
+    elif command == 'l':
+        return doc_list()
+    elif command == 'a':
+        return doc_add(input('Введите номер полки для хранения: '), input('Введите тип документа: '),
+                       input('Введите номер документа: '), input('Введите имя владельца документа: '))
     # elif command == 'd':
     #     return doc_delete()
     # elif command == 'm':
